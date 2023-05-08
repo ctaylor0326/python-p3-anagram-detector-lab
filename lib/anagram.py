37642c64ba6354056e083650aade42f02b6aae9c
CahenTaylor <cahentaylor@gmail.com>
2023-05-08 10:07:08 -0600

    passed

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,11 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, word_list):
+        match_list = []
+        for item in word_list: 
+            if sorted([letter for letter in self.word]) == sorted([letter for letter in item]):
+                match_list.append(item)
+        return match_list
+        
